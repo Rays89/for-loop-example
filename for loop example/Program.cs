7f60b58b3b94047735266a95be3c4fec3d8d84ea
Rays89 <aracheal795@gmail.com>
2018-04-25 11:55:50 -0400

    Added an example of a for loop

---
+++ for loop example/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace for_loop_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int sum = 0;
+
+            for (int i = 1; i <= 5000; i++)
+            {
+                 sum += i;
+            }
+            
+            Console.WriteLine(sum);
+
+
+
+
+
+
+
+
+
+        }
+
+       
+            
+    }
+}
+ 
